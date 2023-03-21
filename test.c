84897d2f7c6f9b2cd91a9894e396dffce2a1ad52
etvomq0023 <2943300264@qq.com>
2023-03-22 00:27:22 +0800

    VSCode 配置源文件

---
+++ test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    printf("Hello World!\n");
+    printf("你好世界！\n");
+    system("pause");    // 防止运行后自动退出，需头文件stdlib.h
+    return 0;
+}
+//————————————————
+//版权声明：本文为CSDN博主「wanna be friend」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+//原文链接：https://blog.csdn.net/m0_59286424/article/details/128270328
